885475bc325d15a8f3696b3ee0ed2c08340380c6
MarinFDM <96052178+MarinFDM@users.noreply.github.com>
2024-01-23 13:36:03 +0100

    Update tri.py

--- tri.py
+++ tri.py
@@ -2,10 +2,30 @@ def tri(list):
     for i in range(len(list)):
         val = list[i]
         pos = i
-        while pos>0 and list[pos-1] > val:
+        while pos>0 and list[pos-1] < val:
             list[pos] = list[pos-1]
             pos-=1
         list[pos] = val
     return list
 
-print(tri([1,3,5,76,877,456,34,23,56,0]))
+def tri_bulle(liste):
+    n = len(liste)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            print(liste,i,j )
+            if liste[j] >liste[j+1]:
+                liste[j], liste[j+1] = liste[j+1], liste[j]
+                print(liste,i,j )
+    return liste
+
+def tri_sellection(Liste):
+    trie1 = 0
+    trie2=0
+    a=0
+    b=len(Liste)
+    Liste=[]
+    for i in range(len(Liste[a, b])):
+        if Liste[i] < Liste[i+1]:
+            trie1 = Liste[i]
+        trie1, trie2 = Liste[a], Liste[i]
+
