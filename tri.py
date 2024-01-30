951d129ed4e3457b4811a212e706fea3a747657d
MarinFDM <96052178+MarinFDM@users.noreply.github.com>
2024-01-30 14:42:06 +0100

    Update tri.py

--- tri.py
+++ tri.py
@@ -57,3 +57,35 @@ def tri_sellectionparlafin(Liste):
         b-=1
     return Liste
 print(tri_sellectionparlafin([9, 5, 8, 6, 4,-2, 15, 88, -25, 80, 33, 60, 2, 5, 505]))
+
+from math import*
+# def est_present_dans(tableau_trié, valeur):
+#     while len(tableau_trié) != 1:
+#         a = len(tableau_trié)//2
+#         if valeur <= tableau_trié[a]:
+#             tableau_trié = tableau_trié[0:a]
+#         else :
+#             tableau_trié = tableau_trié[a+1:len(tableau_trié)]
+#     if valeur == tableau_trié[0]:
+#         result = True
+#     else:
+#         result = False
+#     return result , tableau_trié
+
+# print(est_present_dans([2,3,5,5,6,7],1))
+
+def fctn(x):
+    result = cos(x)-x
+    return result
+
+def rech_fct_dicho(a:int,b:int,p):
+    mid = 0.0
+    while b-a>10**-p:
+        mid = (a+b)/2
+        if fctn(mid) > 0.0:
+            a = mid
+        else:
+            b = mid
+    mid = round(mid,4)
+    return mid
+print(rech_fct_dicho(0,4,4))
